f325ea96741570ffd8159edec1b8caeea39335da
Mitchel Parish <mitchelparish@gmail.com>
2019-01-23 17:13:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,30 @@
 class School
   attr_accessor :name, :roster
 
-def initialize(name)
-  @name = name
-  @roster = {}
-end
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
 
-def add_student(name, grade)
-  if @roster.has_key?(grade)
-    @roster[grade] << name
-  else
-    @roster[grade] = [name]
-    # self.roster[grade] ||= []
-    # if grade already exists, do nothing.
-    # if grade does not exist, create it.
-    # follow-up either way by adding name to grade.
-end
-end
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+        # self.roster[grade] ||= []
+        # if grade already exists, do nothing.
+        # if grade does not exist, create it.
+        # follow-up either way by adding name to grade.
+    end
+  end
 
-def grade(grade)
-  @roster[grade]
-end
+  def grade(grade)
+    @roster[grade]
+  end
 
-def sort
-  self.roster.each do |grade, students|
+  def sort
+    self.roster.each do |grade, students|
     students.sort!
   end
 end
-
-end
+end 
